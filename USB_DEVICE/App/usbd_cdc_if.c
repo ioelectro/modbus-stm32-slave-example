ad4c6967fef0b834c1bcece68e8d739d5abfb7f7
Esmaeill <liyanboy74@gmail.com>
2023-04-01 12:54:27 +0330

    update code

--- USB_DEVICE/App/usbd_cdc_if.c
+++ USB_DEVICE/App/usbd_cdc_if.c
@@ -258,9 +258,10 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
   */
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
+  /* USER CODE BEGIN 6 */
   uint32_t i;
   for(i=0;i<*Len;i++)FIFO_Add(Buf[i]);
-  /* USER CODE BEGIN 6 */
+
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
   return (USBD_OK);
